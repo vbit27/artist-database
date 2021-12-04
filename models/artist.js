e77cac3cc4d63c13fe1c9c86353075ad7161c803
Vasil Bit <vasil.bituni@gmail.com>
2021-12-04 11:44:39 +0100

    Add artist schema

---
+++ models/artist.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let ArtistSchema = new Schema({
+  first_name: {type: String, minlength: 1, maxlength: 50, required=true},
+  family_name: {type: String, minlength: 1, maxlength: 50, required=true},
+  date_of_birt: {type: Date},
+})
+
+ArtistSchema.virtual('name'
+).get(()=>{
+ return this.first_name + ' ' + this.family_name
+})
+
+ArtistSchema.virtual('url').get(()=>{
+  return 'catalog/artist/' + this._id
+})
