5d8df613567aef233befb3a42f9d24b84207df7d
Vasil Bit <vasil.bituni@gmail.com>
2021-12-08 19:37:53 +0100

    Add to do

--- controllers/songController.js
+++ controllers/songController.js
@@ -21,8 +21,37 @@ exports.song_create_post = (req, res) => {
     res.redirect(song.id);
   });
 };
-exports.song_detail = (req, res) => {
-  res.send('NOT - DETAILS');
+
+exports.song_detail = (req, res, next) => {
+  const id = req.param.id;
+
+  // Song.findById(id)
+  //   .populate('artist')
+  //   .exec()
+  //   .then((response) => {
+  //     console.log(response);
+  //     //res.render('song_details', { data: response });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
+  // MAKE PARALLEL ASYNC WORK FOR SONGS
+
+  async.parallel(
+    {
+      song: function (callback) {
+        Song.findById(id, callback);
+      },
+    },
+    function (err, result) {
+      if (err) {
+        next(err);
+      }
+      console.log(result);
+      res.render('song_details', { data: result });
+    }
+  );
 };
 
 exports.song_list = (req, res) => {
